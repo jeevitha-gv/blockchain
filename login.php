71f6cbfcfef9c581e522f025aab1cc822cbd9178
krishna-Eniyavan <krisha@fixnix.co>
2020-03-05 18:14:36 +0530

    login page

--- login.php
+++ login.php
@@ -38,8 +38,21 @@
                } 
   }
 ?>
-<!DOCTYPE html>
 
+
+<!DOCTYPE html>
+<!--
+Template Name: Metronic - Responsive Admin Dashboard Template build with Twitter Bootstrap 4 & Angular 8
+Author: KeenThemes
+Website: http://www.keenthemes.com/
+Contact: support@keenthemes.com
+Follow: www.twitter.com/keenthemes
+Dribbble: www.dribbble.com/keenthemes
+Like: www.facebook.com/keenthemes
+Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
+Renew Support: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
+License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
+-->
 <html lang="en" >
     <!-- begin::Head -->
     <head><!--begin::Base Path (base relative path for assets of this page) -->
@@ -53,11 +66,11 @@
         <!--begin::Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700|Roboto:300,400,500,600,700">        <!--end::Fonts -->
 
-       
+        
                     <!--begin::Page Custom Styles(used by this page) -->
-                             <link href="./assets/css/demo3/pages/login/login-6.css" rel="stylesheet" type="text/css" />
+                             <link href="./assets/css/demo3/pages/login/login-2.css" rel="stylesheet" type="text/css" />
                         <!--end::Page Custom Styles -->
-       
+        
         <!--begin:: Global Mandatory Vendors -->
 <link href="./assets/vendors/general/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" type="text/css" />
 <!--end:: Global Mandatory Vendors -->
@@ -91,9 +104,15 @@
 <link href="./assets/vendors/custom/vendors/flaticon/flaticon.css" rel="stylesheet" type="text/css" />
 <link href="./assets/vendors/custom/vendors/flaticon2/flaticon.css" rel="stylesheet" type="text/css" />
 <link href="./assets/vendors/general/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css" />
+<!--end:: Global Optional Vendors -->
 
+<!--begin::Global Theme Styles(used by all pages) -->
+                    
                     <link href="./assets/css/demo3/style.bundle.css" rel="stylesheet" type="text/css" />
-       
+                <!--end::Global Theme Styles -->
+
+        <!--begin::Layout Skins(used by all pages) -->
+                <!--end::Layout Skins -->
 
         <link rel="shortcut icon" href=" ./assets/media/company-logos/whistle.png" />
     </head>
@@ -103,144 +122,105 @@
     <body  class="kt-quick-panel--right kt-demo-panel--right kt-offcanvas-panel--right kt-header--fixed kt-header-mobile--fixed kt-subheader--fixed kt-subheader--enabled kt-subheader--solid kt-aside--enabled kt-aside--fixed kt-page--loading"  >
 
        
-    <!-- begin:: Page -->
-<div class="kt-grid kt-grid--ver kt-grid--root kt-page">
-<div class="kt-grid kt-grid--hor kt-grid--root  kt-login kt-login--v6 kt-login--signin" id="kt_login">
-    <div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--desktop kt-grid--ver-desktop kt-grid--hor-tablet-and-mobile">
-        <div class="kt-grid__item  kt-grid__item--order-tablet-and-mobile-2  kt-grid kt-grid--hor kt-login__aside">
-            <div class="kt-login__wrapper">
-                <div class="kt-login__container">
-                    <div class="kt-login__body">
-                        <div class="kt-login__logo">
-                            <a href="#">
+      <!-- begin:: Page -->
+  <div class="kt-grid kt-grid--ver kt-grid--root kt-page">
+    <div class="kt-grid kt-grid--hor kt-grid--root kt-login kt-login--v2 kt-login--signin" id="kt_login">
+  <div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor" style="background-image: url(./assets/media//bg/bg-11.jpg);">
+    <div class="kt-grid__item kt-grid__item--fluid kt-login__wrapper">
+      <div class="kt-login__container">
+        <div class="kt-login__logo">
+                            <a>
                                 <img src="./assets/media/company-logos/whistle.png" style="height: 100px;width: 100px;">      
                             </a>
                         </div>
-
-                        <div class="kt-login__signin">
-                            <div class="kt-login__head">
-                                <h3 class="kt-login__title">Are you Admin, Investigator, Idea or Reviewer</h3>
-                                <h5>Sign In</h5>
-                            </div>
-                            <div class="kt-login__form">
-                                <form class="kt-form" method="post" action="">
-                                    <div class="form-group">
-                                        <input class="form-control" type="text" placeholder="Email" id="email" name="email">
-                                    </div>
-                                    <div class="form-group">
-                                        <input class="form-control form-control-last" type="password" id="password" placeholder="Password" name="password">
-                                    </div>
-                                   <!--  <div class="kt-login__extra">
-                                        <label class="kt-checkbox">
-                                            <input type="checkbox" name="remember"> Remember me
-                                            <span></span>
-                                        </label>                        
-                                        <a href="javascript:;" id="kt_login_forgot">Forget Password ?</a>
-                                    </div> -->
-                                    <div class="kt-login__actions">
-                                        <input type="submit" type="submit" name="login" class="btn btn-brand btn-pill btn-elevate"><!-- Sign In  -->
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-
-                       
-
-                       
-                    </div>
-                </div>
-               <!--  <div class="kt-login__account">
-                    <span class="kt-login__account-msg">
-                        Don't have an account yet ?
-                    </span>&nbsp;&nbsp;
-                    <a href="signup.php">Sign Up!</a>
-                </div> -->
+        <div class="kt-login__signin">
+          <div class="kt-login__head">
+            <h3 class="kt-login__title">Are you Admin, Investigator, Idea or Reviewer</h3>
+          </div>
+          <form class="kt-form" method="post" action="">
+            <div class="input-group">
+              <input class="form-control" type="text" style="color: white;" placeholder="Email" id="email" name="email" autocomplete="off">
             </div>
-        </div>
-
-        <div class="kt-grid__item kt-grid__item--fluid kt-grid__item--center kt-grid kt-grid--ver kt-login__content" style="background-image: url(./assets/media/bg/data.jpg);">
-            <div class="kt-login__section">
-                <div class="kt-login__block">
-                    
-                    <div id="fly-in">  
-                    <div><span>whistleblowing is </span>the disclosure of wrong doing</div>
-                    <div>whistleblowing is not<span>disloyalty</span></div>
-                    <div>Whistleblowing can<span>alert you to issues that otherwise go untreated</span></div>
-                    <div><span>whistleblowers can be</span>your greatest assets if you let them</div>
-                    <div><span>whistleblowers are not</span>Liabilties -unless you make them so</div>
-                    </div>
-                   
-
-                </div>
+            <div class="input-group">
+              <input class="form-control" type="password" style="color: white; border-color: white;" placeholder="Password" name="password" id="password">
             </div>
+            <!-- <div class="row kt-login__extra">
+              <div class="col">
+                <label class="kt-checkbox">
+                  <input type="checkbox" name="remember"> Remember me
+                  <span></span>
+                </label>
+              </div>
+              <div class="col kt-align-right">
+                <a href="javascript:;" id="kt_login_forgot" class="kt-link kt-login__link">Forget Password ?</a>
+              </div>
+            </div> -->
+            <div class="kt-login__actions">
+              <button type="submit"  name="login" class="btn btn-pill kt-login__btn-primary">Sign In</button>
+            </div>
+          </form>
         </div>
+        <!-- <div class="kt-login__signup">
+          <div class="kt-login__head">
+            <h3 class="kt-login__title">Sign Up</h3>
+            <div class="kt-login__desc">Enter your details to create your account:</div>
+          </div>
+          <form class="kt-login__form kt-form" action="">
+            <div class="input-group">
+              <input class="form-control" type="text" placeholder="Fullname" name="fullname">
+            </div>
+            <div class="input-group">
+              <input class="form-control" type="text" placeholder="Email" name="email" autocomplete="off">
+            </div>
+            <div class="input-group">
+              <input class="form-control" type="password" placeholder="Password" name="password">
+            </div>
+            <div class="input-group">
+              <input class="form-control" type="password" placeholder="Confirm Password" name="rpassword">
+            </div>
+            <div class="row kt-login__extra">
+              <div class="col kt-align-left">
+                <label class="kt-checkbox">
+                  <input type="checkbox" name="agree">I Agree the <a href="#" class="kt-link kt-login__link kt-font-bold">terms and conditions</a>.
+                  <span></span>
+                </label>
+                <span class="form-text text-muted"></span>
+              </div>
+            </div>
+            <div class="kt-login__actions">
+              <button id="kt_login_signup_submit" class="btn btn-pill kt-login__btn-primary">Sign Up</button>&nbsp;&nbsp;
+              <button id="kt_login_signup_cancel" class="btn btn-pill kt-login__btn-secondary">Cancel</button>
+            </div>
+          </form>
+        </div>
+        <div class="kt-login__forgot">
+          <div class="kt-login__head">
+            <h3 class="kt-login__title">Forgotten Password ?</h3>
+            <div class="kt-login__desc">Enter your email to reset your password:</div>
+          </div>
+          <form class="kt-form" action="">
+            <div class="input-group">
+              <input class="form-control" type="text" placeholder="Email" name="email" id="kt_email" autocomplete="off">
+            </div>
+            <div class="kt-login__actions">
+              <button id="kt_login_forgot_submit" class="btn btn-pill kt-login__btn-primary">Request</button>&nbsp;&nbsp;
+              <button id="kt_login_forgot_cancel" class="btn btn-pill kt-login__btn-secondary">Cancel</button>
+            </div>
+          </form>
+        </div> -->
+        <!-- <div class="kt-login__account">
+          <span class="kt-login__account-msg">
+            Don't have an account yet ?
+          </span>&nbsp;&nbsp;
+          <a href="javascript:;" id="kt_login_signup" class="kt-link kt-link--light kt-login__account-link">Sign Up</a>
+        </div> -->
+      </div>  
     </div>
-</div>         </div>
-
+  </div>
+</div>  
+  </div>
+  
 <!-- end:: Page -->
-
-
-
-<style type="text/css">
-    @import url('https://fonts.googleapis.com/css?family=Montserrat:700');
-
-
-
-/*body {
-  background-color: #111;
-  background-image: radial-gradient(#333, #111);
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  color: #fff;
-  text-align: center;
-  width: 100vw;
-  font-weight: 700;
-  overflow: hidden;
-  font-family: 'Montserrat', sans-serif;
-}*/
-
-#fly-in {
-  font-size: 4em;
-  margin: 40vh auto;
-  height: 20vh; 
-  text-transform: uppercase;
-}
-
-#fly-in span {
-  display: block;
-  font-size: .4em;
-  opacity: .8;
-  color: #F2F2F2;
-}
-
-#fly-in div {
- position: fixed; <!--codepen.io/h-kod/-->
-  margin: 5vh 0;
-  opacity: 0;
-  right:-25vw;
-  width: 85vw;
-  animation: switch 32s linear infinite;
-  color: white;
-}
-
-#fly-in div:nth-child(2) { animation-delay: 25s}
-#fly-in div:nth-child(3) { animation-delay: 5s}
-#fly-in div:nth-child(4) { animation-delay: 12s}
-#fly-in div:nth-child(5) { animation-delay: 16s}
-#fly-in div:nth-child(6) { animation-delay: 20s}
-#fly-in div:nth-child(7) { animation-delay: 24s}
-#fly-in div:nth-child(8) { animation-delay: 28s}
-
-@keyframes switch {
-    0% { opacity: 0;filter: blur(20px); transform:scale(12)}
-    3% { opacity: 1;filter: blur(0); transform:scale(1)}
-    10% { opacity: 1;filter: blur(0); transform:scale(.9)}
-    13% { opacity: 0;filter: blur(10px); transform:scale(.1)}
-    80% { opacity: 0}
-    100% { opacity: 0}
-}
-</style>
 
 
         <!-- begin::Global Config(global config for global JS sciprts) -->
@@ -249,7 +229,7 @@
         </script>
         <!-- end::Global Config -->
 
-    <!--begin:: Global Mandatory Vendors -->
+      <!--begin:: Global Mandatory Vendors -->
 <script src="./assets/vendors/general/jquery/dist/jquery.js" type="text/javascript"></script>
 <script src="./assets/vendors/general/popper.js/dist/umd/popper.js" type="text/javascript"></script>
 <script src="./assets/vendors/general/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
@@ -320,11 +300,11 @@
 <!--end:: Global Optional Vendors -->
 
 <!--begin::Global Theme Bundle(used by all pages) -->
-         
-      <script src="./assets/js/demo3/scripts.bundle.js" type="text/javascript"></script>
-<!--end::Global Theme Bundle -->
+               
+             <script src="./assets/js/demo3/scripts.bundle.js" type="text/javascript"></script>
+        <!--end::Global Theme Bundle -->
 
-       
+        
                     <!--begin::Page Scripts(used by this page) -->
                             <script src="./assets/js/demo3/pages/login/login-general.js" type="text/javascript"></script>
                         <!--end::Page Scripts -->
